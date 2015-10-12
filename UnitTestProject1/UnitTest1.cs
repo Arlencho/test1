4d7079e20cf7412102977e6bcc6a8fb3dc485aa0
Arlen Rios <arlenrios@icloud.com>
2015-10-12 10:50:08 +0200

    Added a test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ooad.Domain;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            Class1 sut = new Class1();
+            int i1 = 5;
+            int i2 = 5;
+            //Act
+            sut.IsEqual(i1, i2);
+            //Assert
+            Assert.IsTrue(true);
+
+        }
+    }
+}
---
+++ ooad.Domain/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooad.Domain
+{
+    public class Class1
+    {
+        public bool result;
+        public void IsEqual(int i1, int i2)
+        {
+            result = (i1==i2);
+        }
+    }
+}
